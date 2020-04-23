89dbd5628fc44d83cf68cb8020e388c7d644342d
Anil <anil_gurung15@yahoo.com>
2020-04-22 21:55:04 -0700

    setup database ,models and controllers

---
+++ producers-blog/app/controllers/application_controller.rb
@@ -0,0 +1,40 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "ninja"
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find_by(id: session[:user_id])
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login"
+      end
+    end
+
+    def redirect_if_logged_in
+      if logged_in?
+        redirect "/posts"
+      end
+    end
+
+
+  end
+
+end
---
+++ producers-blog/app/models/producer.rb
@@ -0,0 +1,5 @@
+class Producer < ActiveRecord::Base
+    has_many :movies
+    has_many :posts
+    has_secure_password
+end
